3aa5089ed513f246f90758778b996bb94c1fc060
FFC03 <FFC03@Feli.home>
2025-12-10 10:36:05 -0500

    Completed all class methods (error because News, RealNews and FakeNews classes aren't completed yet)

--- src/classes/Answer.java
+++ src/classes/Answer.java
@@ -12,9 +12,17 @@ public class Answer {
     private boolean correctType;
     private boolean correctError;
     public Answer(News news, String identifiedType){
-        
+        correctType = news.getType().equalsIgnoreCase(identifiedType);
+        correctError = news instanceof RealNews;
     }
     public Answer(News news, String identifiedType, String identifiedError){
-        
+        correctType = news.getType().equalsIgnoreCase(identifiedError);
+        correctError = news instanceof RealNews? false : ((FakeNews) news).getSourceOfError().equals(identifiedError);
+    }
+    public boolean getTypeCorrect(){
+        return correctType;
+    }
+    public boolean getErrorCorrect(){
+        return correctError;
     }
 }
