158c2489cbdba0371cb085214320a261ff7feabd
342889201 <342889201@C-43M5JB2.tdsb.on.ca>
2025-12-10 11:15:16 -0500

    Created basic News class information

--- src/classes/News.java
+++ src/classes/News.java
@@ -9,5 +9,30 @@ package classes;
  * @author FFC03
  */
 public class News {
-    
+    private String headline;
+    private String contents;
+    private String author;
+    private String date;
+    private String newsSource;
+    private String type;
+    private String category;
+    public News(String headline, String contents, String author, String date, String newsSource, String type, String category){
+        this.headline = headline;
+        this.contents = contents;
+        this.author = author;
+        this.date = date;
+        this.newsSource = newsSource;
+        this.type = type;
+        this.category = category;
+    }
+    public String information(){
+        
+    }
+    public String getType(){
+        
+    }
+    @Override
+    public String toString(){
+        
+    }
 }
