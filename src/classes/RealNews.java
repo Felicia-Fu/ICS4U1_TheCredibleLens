f284a37fef7fa3aefd4a57ba98af52ce7bc0dd03
342889201 <342889201@C-43M5JB2.tdsb.on.ca>
2025-12-12 10:58:16 -0500

    Added comments

--- src/classes/RealNews.java
+++ src/classes/RealNews.java
@@ -6,14 +6,32 @@ package classes;
 
 /**
  *
- * @author FFC03
+ * @author Jia Xin Li
  */
 public class RealNews extends News{
+    //Created variables
     private String url;
+    
+    /**
+     * Constructor for RealNews Class
+     * @param type
+     * @param category
+     * @param headline
+     * @param contents
+     * @param author
+     * @param date
+     * @param newsSource
+     * @param url
+     */
     public RealNews(String type, String category, String headline, String contents, String author, String date, String newsSource, String url){
         super(type, category, headline, contents, author, date, newsSource);
         this.url = url;
     }
+    
+    /**
+     * Override method for constant output
+     * @return
+     */
     @Override
     public String information(){
         return """
