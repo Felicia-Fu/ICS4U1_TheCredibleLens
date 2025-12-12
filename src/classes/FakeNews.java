a5c5d4af3e6ae7648d0a3e7d4015394e16bb685d
connieyu <connieyu@localhost>
2025-12-12 10:44:57 -0500

    finished fake news class

--- src/classes/FakeNews.java
+++ src/classes/FakeNews.java
@@ -9,5 +9,19 @@ package classes;
  * @author FFC03
  */
 public class FakeNews extends News{
+    private String sourceOfError;
     
+    public FakeNews(String type, String category, String headline, String contents, String author, String date, String newsSource, String sourceOfError){
+        super(type, category, headline, contents, author, date, newsSource);
+        this.sourceOfError = sourceOfError;
+    }
+    
+    public String getSourceOfError(){
+        return sourceOfError;
+    }
+    
+    @Override
+    public String information(){
+        return "This is fake news. Source of Error: " + sourceOfError;
+    }
 }
